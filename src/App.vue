1b50be292255ee22ea96c6e5ff806dd881a4312c
Enayatullal Shadab <Enayatullah_Shadab@yahoo.com>
2021-08-04 08:29:07 -0600

    reactive

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,41 @@
+/* eslint-disable no-unused-vars */
 <template>
  
   <h1> <img alt="Vue logo" src="./assets/logo.png">ue Demo</h1>
+  <h2>{{name}}</h2>
+  <p> {{state.value}} </p>
+  <button @click="count">Click me</button>
+
+
 </template>
 
 <script>
+import {reactive} from 'vue'
 
 export default{
+  setup(){
+   
+    // eslint-disable-next-line no-unused-vars
+    const state = reactive({
+      value: 0
+    })
+    
+    return{
+      name: "Shadab",
+      value:0,
+      count(){
+        this.value++
+        console.log("Clicked  "+ state.value)
+      }
+
+    }
+  }
+
 
 }
 </script>
 <style>
+
 #app{
   text-align: center;
 }
