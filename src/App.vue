743139de1dfe1826ee735df4ce93b54465eab6e8
Enayatullal Shadab <Enayatullah_Shadab@yahoo.com>
2021-08-04 07:56:05 -0600

    Exercise Vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ 
+  <h1> <img alt="Vue logo" src="./assets/logo.png">ue Demo</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+export default{
+
 }
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+#app{
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
+img{
+  height: 1em;
+}
+
 </style>
